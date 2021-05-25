d34949a4a82d3b8c27ac63819c1bcfa9985c9dc6
iluzych <ieva.daugvilaite@gmail.com>
2021-05-25 18:00:37 +0300

    fullname and sum tests

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+
 const Validation = require('./js/Validation');
 
 const v = new Validation();
 
-console.log(v.isValidFirstName('Vardenis'), true);
-console.log(v.isValidLastName('Pavardenis'), true);
-console.log(v.isValidEmail('demo@example.com'), true);
-console.log(v.isValidMessage('Some message'), true);
-console.log(v.isValidPhoneNumber(+37061234567), true);
-console.log(v.isValidMonthName('Sausis'), true);
-console.log(v.isValidMonthName('January'), true);
-console.log(v.isValidWeekdayName('Pirmadienis'), true);
-console.log(v.isValidWeekdayName('Monday'), true);
-console.log(v.isValidPersonID(39912311234), true);
+console.assert(v.isValidFirstName('Vardenis') === true, 'ERROR');
+console.assert(v.isValidLastName('Pavardenis') === true, 'ERROR');
+console.assert(v.isValidEmail('demo@example.com') === true, 'ERROR: email');
+console.assert(v.isValidMessage('Some message') === true, 'ERROR');
+console.assert(v.isValidPhoneNumber(+37061234567) === true, 'ERROR');
+console.assert(v.isValidMonthName('Sausis') === true, 'ERROR');
+console.assert(v.isValidMonthName('January') === true, 'ERROR');
+console.assert(v.isValidWeekdayName('Pirmadienis') === true, 'ERROR');
+console.assert(v.isValidWeekdayName('Monday') === true, 'ERROR');
+console.assert(v.isValidPersonID(39912311234) === true, 'ERROR');
---
+++ js/sum/sum.js
@@ -0,0 +1,8 @@
+function sum(a, b) {
+    if (a > b) {
+        return a + b;
+    }
+    return b + a;
+}
+
+module.exports = sum;
